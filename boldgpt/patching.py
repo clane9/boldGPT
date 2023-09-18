142995bc63157c85e0bb94c3cb0d4f6cbf0006bf
Connor Lane <connor.lane858@gmail.com>
2023-09-17 21:16:24 -0400

    Move patch reordering into `MaskedPatchify`

--- boldgpt/patching.py
+++ boldgpt/patching.py
@@ -4,32 +4,54 @@ import torch
 from einops.layers.torch import Rearrange
 from torch import nn
 
+ORDERINGS = ("raster", "radial", "random")
+
 
 class MaskedPatchify(nn.Module):
     """
     Patchify images restricted to a mask, shape (H, W).
+
+    Supports different orderings for the patches:
+
+    - raster: original image raster order
+    - radial: patches sorted by distance from mask centroid
+    - random: fixed random patch ordering
     """
+
+    # TODO: could add support for a custom ordering provided via a "priority" map
 
     mask: torch.Tensor
     patch_mask: torch.Tensor
     patch_indices: torch.Tensor
+    order: torch.Tensor
 
-    def __init__(self, mask: torch.Tensor, patch_size: int = 8):
+    def __init__(
+        self,
+        mask: torch.Tensor,
+        patch_size: int = 8,
+        ordering: str = "raster",
+    ):
         assert mask.ndim == 2, f"Invalid mask shape {mask.shape}; expected (H, W)"
+        assert (
+            ordering in ORDERINGS
+        ), f"Unknown ordering {ordering}; expected one of {ORDERINGS}"
 
         super().__init__()
         mask = torch.as_tensor(mask > 0, dtype=torch.bool)
         H, W = mask.shape
+        device = mask.device
 
         self.patch_size = patch_size
+        self.ordering = ordering
         self.height = H
         self.width = W
         self.dim = patch_size**2
 
-        padding = _infer_padding(H, W, multiple=patch_size)
-        self.padding = padding
-
+        # Padding to multiple of patch size
+        self.padding = padding = _infer_padding(H, W, multiple=patch_size)
         self.pad = nn.ConstantPad2d(padding, 0.0)
+
+        # Patchify/unpatchify using einops
         self.to_patches = Rearrange(
             "b (h p1) (w p2) -> b (h w) (p1 p2)", p1=patch_size, p2=patch_size
         )
@@ -42,19 +64,37 @@ class MaskedPatchify(nn.Module):
         )
 
         # Patchified mask
-        patch_mask = mask[None, ...]
-        patch_mask = self.pad(patch_mask)
-        patch_mask = self.to_patches(patch_mask).squeeze(0)
+        patch_mask = self.forward_raw(mask[None, ...]).squeeze(0)
         self.num_total_patches = len(patch_mask)
 
         # Indices of patches intersecting the mask
         patch_indices = torch.where(torch.any(patch_mask, dim=1))[0]
-        patch_mask = patch_mask[patch_indices]
         self.num_patches = len(patch_indices)
+
+        # Patch order
+        if ordering == "raster":
+            order = torch.arange(self.num_patches, device=device)
+        elif ordering == "radial":
+            dist = _distance_map(mask)
+            patch_dist = self.forward_raw(dist[None, ...]).squeeze(0)
+            patch_dist = patch_dist[patch_indices]
+            patch_dist = torch.mean(patch_dist, dim=1)
+            order = torch.argsort(patch_dist)
+        elif ordering == "random":
+            order = torch.randperm(self.num_patches, device=device)
+        else:
+            raise ValueError(
+                f"Unknown ordering {ordering}; expected one of {ORDERINGS}"
+            )
+
+        # Apply ordering
+        patch_indices = patch_indices[order]
+        patch_mask = patch_mask[patch_indices]
 
         self.register_buffer("mask", mask)
         self.register_buffer("patch_mask", patch_mask)
         self.register_buffer("patch_indices", patch_indices)
+        self.register_buffer("order", order)
 
     @property
     def interior_mask(self) -> torch.Tensor:
@@ -63,39 +103,33 @@ class MaskedPatchify(nn.Module):
         """
         return torch.all(self.patch_mask, dim=-1)
 
-    def forward(self, images: torch.Tensor) -> torch.Tensor:
-        """
-        Convert images, shape (N, H, W), to patches, shape (N, L, D).
-        """
-        squeeze = False
-        if images.ndim == 2:
-            images = images[None, ...]
-            squeeze = True
-
+    def forward_raw(self, images: torch.Tensor) -> torch.Tensor:
+        assert images.ndim == 3, "Invalid image shape; expected (B, H, W)"
         images = self.pad(images)
         patches = self.to_patches(images)
-        patches = patches[..., self.patch_indices, :]
-
-        if squeeze:
-            patches = patches.squeeze(0)
         return patches
 
-    def inverse(self, patches: torch.Tensor) -> torch.Tensor:
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
         """
-        Convert patches, shape (N, L, D), back to images, shape (N, H, W).
+        Convert images, shape (B, H, W), to patches, shape (B, N, C).
         """
-        squeeze = False
-        if patches.ndim == 2:
-            patches = patches[None, ...]
-            squeeze = True
+        patches = self.forward_raw(images)
+        patches = patches[..., self.patch_indices, :]
+        return patches
 
-        N, _, D = patches.shape
+    def inverse(self, patches: torch.Tensor, apply_mask: bool = True) -> torch.Tensor:
+        """
+        Convert patches, shape (B, N, C), back to images, shape (B, H, W).
+        """
+        assert patches.ndim == 3, "Invalid patches shape; expected (B, N, C)"
+        assert patches.shape[1] == self.num_patches, "Invalid patches shape"
+        B = patches.shape[0]
         dtype = patches.dtype
         device = patches.device
 
         # Inverse masking
         expanded = torch.zeros(
-            (N, self.num_total_patches, D), dtype=dtype, device=device
+            (B, self.num_total_patches, self.dim), dtype=dtype, device=device
         )
         expanded[..., self.patch_indices, :] = patches
 
@@ -104,14 +138,26 @@ class MaskedPatchify(nn.Module):
         left, _, top, _ = self.padding
         images = images[..., top : top + self.height, left : left + self.width]
 
-        if squeeze:
-            images = images.squeeze(0)
+        if apply_mask:
+            images = images * self.mask
+        return images
+
+    def inverse_vector(
+        self, values: torch.Tensor, apply_mask: bool = True
+    ) -> torch.Tensor:
+        """
+        Reconstruct a patch map from a vector of patch values.
+        """
+        assert values.ndim == 2, "Invalid values shape"
+        patches = values[..., None].expand(-1, -1, self.dim)
+        images = self.inverse(patches, apply_mask=apply_mask)
         return images
 
     def extra_repr(self) -> str:
         return (
             f"patch_size={self.patch_size}, height={self.height}, "
-            f"width={self.width}, num_patches={self.num_patches}"
+            f"width={self.width}, num_patches={self.num_patches}, "
+            f"ordering={self.ordering}"
         )
 
 
@@ -126,3 +172,19 @@ def _infer_padding(height: int, width: int, multiple: int = 8):
     # [left, right, top, bottom]
     padding = [p for pad in reversed(padding) for p in [pad // 2, pad - pad // 2]]
     return padding
+
+
+def _distance_map(mask: torch.Tensor) -> torch.Tensor:
+    """
+    Compute the distance map from the mask centroid.
+    """
+    mask = mask
+    device = mask.device
+
+    y = torch.arange(mask.shape[0], dtype=torch.float32, device=device)
+    x = torch.arange(mask.shape[1], dtype=torch.float32, device=device)
+    coord = torch.stack(torch.meshgrid(y, x, indexing="ij"), dim=-1)
+
+    centroid = coord[mask].mean(dim=0)
+    dist = torch.linalg.norm(coord - centroid, axis=-1)
+    return dist
--- tests/test_patching.py
+++ tests/test_patching.py
@@ -3,13 +3,14 @@ import logging
 import pytest
 import torch
 
-from boldgpt.patching import MaskedPatchify
+from boldgpt.patching import ORDERINGS, MaskedPatchify
 
 
-def test_patchify(mask: torch.Tensor, img: torch.Tensor):
+@pytest.mark.parametrize("ordering", ORDERINGS)
+def test_patchify(mask: torch.Tensor, img: torch.Tensor, ordering: str):
     img = mask * img
 
-    patchify = MaskedPatchify(mask, patch_size=8)
+    patchify = MaskedPatchify(mask, patch_size=8, ordering=ordering)
     logging.info("%s", patchify)
 
     patches = patchify(img)
@@ -18,7 +19,7 @@ def test_patchify(mask: torch.Tensor, img: torch.Tensor):
     img2 = patchify.inverse(patches)
     assert torch.allclose(img, img2)
 
-    mask2 = patchify.inverse(patchify.patch_mask)
+    mask2 = patchify.inverse(patchify.patch_mask[None, ...]).squeeze(0)
     assert torch.allclose(mask, mask2)
 
 
